3ae8e5a3c999dfa6b75f5852e57ee4ba8ce4996b
TehAbyss <tehabyss@gmail.com>
2019-10-05 16:53:43 -0700

    Add project spawner script and enemy movement script
    Add spawner to GameObject to fire bullets

---
+++ Assets/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+
+    private float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    public void SetMoveDirection(Vector2 direction)
+    {
+        moveDirection = direction;
+    }
+
+    public void SetMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+}
