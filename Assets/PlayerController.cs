2f9d766f72db50d149b04a0b9f58eb89834837a8
Gary Wong <gawong@microsoft.com>
2019-10-04 23:32:02 -0700

    Add player prefab

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float Speed = 7;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(xAxis, yAxis, 0);
+        tempVect = tempVect.normalized * Speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+        //transform.Translate(tempVect);
+    }
+}
