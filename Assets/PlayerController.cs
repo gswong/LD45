b70483d86a395faac5addf4de28403876cc694de
Gary Wong <gawong@microsoft.com>
2019-10-05 13:08:58 -0700

    Add wall collision

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float Speed = 7;
+
+    // Externally control the speed
+    public float MaxSpeed = 7;
+
+    // Internally computed speed
+    private float speed;
+
     private Rigidbody2D rb;
+
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        speed = MaxSpeed;
     }
 
     // Update is called once per frame
@@ -16,8 +25,13 @@ public class PlayerController : MonoBehaviour {
         float yAxis = Input.GetAxis("Vertical");
 
         Vector3 tempVect = new Vector3(xAxis, yAxis, 0);
-        tempVect = tempVect.normalized * Speed * Time.deltaTime;
+
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
         rb.MovePosition(rb.transform.position + tempVect);
         //transform.Translate(tempVect);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
     }
 }
