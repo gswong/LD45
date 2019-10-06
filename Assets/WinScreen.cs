69e4cfbe1c4af852efe298b6330a04aa9fa01f45
Gary Wong <gawong@microsoft.com>
2019-10-06 14:00:22 -0700

    Add delay for continuing from win screen

--- Assets/WinScreen.cs
+++ Assets/WinScreen.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour
 {
+    private float startTime;
+    private float delayTime;
+    private Text t;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
+        delayTime = startTime + 3f;
+        t = GetComponent<Text>();
+        t.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey) {
-            SceneManager.LoadScene("GameplayScene");
+        if (Time.time > delayTime) {
+            t.text = "Press any key to play again.";
+            if (Input.anyKey) {
+                SceneManager.LoadScene("GameplayScene");
+            }
         }
+        t.color = Color.Lerp(Color.black, Color.white, Time.time - delayTime);
     }
 }
