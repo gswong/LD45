2a31a58c29f0f6ab1494baa7390abf74119d5e9e
Gary Wong <gawong@microsoft.com>
2019-10-06 17:17:16 -0700

    Link together all scenes

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class CameraController : MonoBehaviour
 {
     GameObject player;
+    private Scene scene;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "BossScene") {
+            GameObject.Find("Goal").GetComponent<Text>().text = "Defeat the boss!";
+        }
     }
 
     // Update is called once per frame
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -8,11 +8,13 @@ public class UIController : MonoBehaviour
 {
     private float startTime;
     private float delayTime;
+    private Scene scene;
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
         delayTime = startTime + 2f;
+        scene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
@@ -24,7 +26,10 @@ public class UIController : MonoBehaviour
         PlayerController player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         GameObject livesValue = GameObject.Find("LivesValue");
         livesValue.GetComponent<Text>().text = player.Lives.ToString();
-        if (enemiesValue.GetComponent<Text>().text == "0") {
+        if (scene.name == "GameplayScene" && enemiesValue.GetComponent<Text>().text == "0") {
+            SceneManager.LoadScene("BossScene");
+        }
+        if (scene.name == "BossScene" && enemiesValue.GetComponent<Text>().text == "0") {
             SceneManager.LoadScene("WinScreen");
         }
         if (Time.time > delayTime) {
